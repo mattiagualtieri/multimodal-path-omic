c15e511a5ac56934d1af093924b55f170196592f
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-08-21 13:48:05 +0300

    Implemented NaCAGAT

--- models/blocks.py
+++ models/blocks.py
@@ -1,14 +1,16 @@
+import torch
 import torch.nn as nn
+import torch.functional as F
 
 
 class AttentionNetGated(nn.Module):
-    def __init__(self, input_dim=256, hidden_dim=256, dropout=True, n_classes=1):
+    def __init__(self, input_dim: int = 256, hidden_dim: int = 256, dropout: bool = True, n_classes: int = 1):
         r"""
         Attention Network with Sigmoid Gating (3 fc layers)
 
         args:
-            L (int): input feature dimension
-            D (int): hidden layer dimension
+            input_dim (int): input feature dimension
+            hidden_dim (int): hidden layer dimension
             dropout (bool): whether to apply dropout (p = 0.25)
             n_classes (int): number of classes
         """
@@ -37,3 +39,68 @@ class AttentionNetGated(nn.Module):
         # N x n_classes
         A = self.attention_c(A)
         return A, x
+
+
+class PreGatingContextualAttentionGate(nn.Module):
+    def __init__(self, dim1: int = 256, dim2: int = 256, dk: int = 256, output_dim: int = 128):
+        r"""
+        Pre-gating and Contextual Attention Gate (PCAG)
+
+        args:
+            dim1 (int): dimension of the first input
+            dim2 (int): dimension of the second input
+            dk (int): hidden layer dimension
+            output_dim (int): dimension of the output
+        """
+        super(PreGatingContextualAttentionGate, self).__init__()
+
+        self.dk = dk
+        self.output_dim = output_dim
+
+        self.fc_q = nn.Linear(dim2, self.dk)
+        self.fc_k = nn.Linear(dim1, self.dk)
+        self.fc_v = nn.Linear(dim1, self.dk)
+
+        self.fc_cag = []
+        for i in range(0, 4):
+            fc = nn.Sequential(nn.Linear(self.dk, self.output_dim), nn.ReLU())
+            self.fc_cag.append(fc)
+
+        self.G = nn.Sequential(nn.ReLU(), nn.LayerNorm(self.output_dim))
+        self.E = nn.Sequential(nn.ReLU(), nn.LayerNorm(self.output_dim))
+        self.fc_c = nn.Sequential(nn.Linear(self.output_dim, self.output_dim), nn.ReLU())
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor):
+        q = self.fc_q(x2)
+        k = self.fc_k(x1)
+        v = self.fc_v(x1)
+
+        qk = torch.matmul(q, k.transpose(-2, -1))
+        p = (torch.matmul(torch.tanh(q), torch.tanh(k.transpose(-2, -1))) + 1) / 2
+        scores = (qk * p) / torch.sqrt(torch.tensor(self.dk, dtype=torch.float32))
+        attention_weights = torch.softmax(scores, dim=-1)
+        q_hat = torch.matmul(attention_weights, v)
+
+        g = self.G(self.fc_cag[0](q) + self.fc_cag[1](q_hat))
+        e = self.E(self.fc_cag[2](q_hat))
+        c = g * e
+
+        c = self.fc_c(c)
+        q = self.fc_cag[3](q)
+
+        return q + c, attention_weights
+
+
+def test_pcag():
+    print('Testing PreGatingContextualAttentionGate...')
+
+    slide = torch.randn((3000, 1024))
+    omics = torch.randn((6, 256))
+
+    block = PreGatingContextualAttentionGate(dim1=1024, dim2=256, dk=256, output_dim=128)
+    output, attention_weights = block(slide, omics)
+    assert output.shape[0] == attention_weights.shape[0] == 6
+    assert output.shape[1] == 128
+    assert attention_weights.shape[1] == 3000
+
+    print('Forward successful')
---
+++ models/nacagat/nacagat.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.blocks import AttentionNetGated, PreGatingContextualAttentionGate
+from models.fusion import BilinearFusion, ConcatFusion, GatedConcatFusion
+
+
+class NarrowGatingContextualAttentionGateTransformer(nn.Module):
+    def __init__(self, omic_sizes: [], model_size: str = 'medium', n_classes: int = 4, dropout: float = 0.25, fusion: str = 'concat', device: str = 'cpu'):
+        super(NarrowGatingContextualAttentionGateTransformer, self).__init__()
+        self.n_classes = n_classes
+        if model_size == 'small':
+            self.model_sizes = [128, 128]
+        elif model_size == 'medium':
+            self.model_sizes = [256, 256]
+        elif model_size == 'big':
+            self.model_sizes = [512, 512]
+
+        # H
+        fc = nn.Sequential(
+            nn.Linear(1024, self.model_sizes[0]),
+            nn.ReLU(),
+            nn.Dropout(dropout)
+        )
+        self.H = fc
+
+        # G
+        omic_encoders = []
+        for omic_size in omic_sizes:
+            fc = nn.Sequential(
+                nn.Sequential(
+                    nn.Linear(omic_size, self.model_sizes[0]),
+                    nn.ELU(),
+                    nn.AlphaDropout(p=dropout, inplace=False)),
+                nn.Sequential(
+                    nn.Linear(self.model_sizes[0], self.model_sizes[1]),
+                    nn.ELU(),
+                    nn.AlphaDropout(p=dropout, inplace=False))
+            )
+            omic_encoders.append(fc)
+        self.G = nn.ModuleList(omic_encoders)
+
+        # Pre-gating and Contextual Attention Gate
+        self.co_attention = PreGatingContextualAttentionGate(dim1=self.model_sizes[1], dim2=self.model_sizes[1], dk=self.model_sizes[1], output_dim=self.model_sizes[1])
+
+        # Path Transformer (T_H)
+        path_encoder_layer = nn.TransformerEncoderLayer(d_model=self.model_sizes[1], nhead=8, dim_feedforward=512, dropout=dropout,
+                                                        activation='relu')
+        self.path_transformer = nn.TransformerEncoder(path_encoder_layer, num_layers=2)
+
+        # WSI Global Attention Pooling (rho_H)
+        self.path_attention_head = AttentionNetGated(n_classes=1, input_dim=self.model_sizes[1], hidden_dim=self.model_sizes[1])
+        self.path_rho = nn.Sequential(*[nn.Linear(self.model_sizes[1], self.model_sizes[1]), nn.ReLU(), nn.Dropout(dropout)])
+
+        # Omic Transformer (T_G)
+        omic_encoder_layer = nn.TransformerEncoderLayer(d_model=self.model_sizes[1], nhead=8, dim_feedforward=512, dropout=dropout,
+                                                        activation='relu')
+        self.omic_transformer = nn.TransformerEncoder(omic_encoder_layer, num_layers=2)
+
+        # Genomic Global Attention Pooling (rho_G)
+        self.omic_attention_head = AttentionNetGated(n_classes=1, input_dim=self.model_sizes[1], hidden_dim=self.model_sizes[1])
+        self.omic_rho = nn.Sequential(*[nn.Linear(self.model_sizes[1], self.model_sizes[1]), nn.ReLU(), nn.Dropout(dropout)])
+
+        # Fusion Layer
+        self.fusion = fusion
+        if self.fusion == 'concat':
+            self.fusion_layer = ConcatFusion(dims=[self.model_sizes[1], self.model_sizes[1]],
+                                             hidden_size=self.model_sizes[1], output_size=self.model_sizes[1], device=device)
+        elif self.fusion == 'bilinear':
+            self.fusion_layer = BilinearFusion(dim1=self.model_sizes[1], dim2=self.model_sizes[1], output_size=self.model_sizes[1])
+        elif self.fusion == 'gated_concat':
+            self.fusion_layer = GatedConcatFusion(dims=[self.model_sizes[1], self.model_sizes[1]],
+                                                  hidden_size=self.model_sizes[1], output_size=self.model_sizes[1], device=device)
+        else:
+            raise RuntimeError(f'Fusion mechanism {self.fusion} not implemented')
+
+        # Classifier
+        self.classifier = nn.Linear(self.model_sizes[1], n_classes)
+
+    def forward(self, wsi, omics):
+        # WSI Fully connected layer
+        # H_bag: (Mxd_k)
+        H_bag = self.H(wsi).squeeze(0)
+
+        # N Omics Fully connected layers
+        G_omic = [self.G[index].forward(omic.type(torch.float32)) for index, omic in enumerate(omics)]
+        # G_bag: (Nxd_k)
+        G_bag = torch.stack(G_omic).squeeze(1)
+
+        # Co-Attention results
+        # H_coattn: Genomic-Guided WSI-level Embeddings (Nxd_k)
+        # A_coattn: Co-Attention Matrix (NxM)
+        H_coattn, A_coattn = self.co_attention(x1=H_bag, x2=G_bag)
+
+        # Set-Based MIL Transformers
+        # Attention is permutation-equivariant, so dimensions are the same (Nxd_k)
+        path_trans = self.path_transformer(H_coattn)
+        omic_trans = self.omic_transformer(G_bag)
+
+        # Global Attention Pooling
+        A_path, h_path = self.path_attention_head(path_trans.squeeze(1))
+        A_path = torch.transpose(A_path, 1, 0)
+        h_path = torch.mm(F.softmax(A_path, dim=1), h_path)
+        # h_path: final WSI embeddings (dk)
+        h_path = self.path_rho(h_path).squeeze()
+
+        A_omic, h_omic = self.omic_attention_head(omic_trans.squeeze(1))
+        A_omic = torch.transpose(A_omic, 1, 0)
+        h_omic = torch.mm(F.softmax(A_omic, dim=1), h_omic)
+        # h_omic: final omics embeddings (dk)
+        h_omic = self.omic_rho(h_omic).squeeze()
+
+        # Fusion Layer
+        # h: final representation (dk)
+        h = self.fusion_layer(h_path, h_omic)
+
+        # Survival Layer
+
+        # logits: classifier output
+        # size   --> (1, 4)
+        # domain --> R
+        logits = self.classifier(h).unsqueeze(0)
+        # hazards: probability of patient death in interval j
+        # size   --> (1, 4)
+        # domain --> [0, 1]
+        hazards = torch.sigmoid(logits)
+        # survs: probability of patient survival after time t
+        # size   --> (1, 4)
+        # domain --> [0, 1]
+        survs = torch.cumprod(1 - hazards, dim=1)
+        # Y: predicted probability distribution
+        # size   --> (1, 4)
+        # domain --> [0, 1] (probability distribution)
+        Y = F.softmax(logits, dim=1)
+
+        attention_scores = {'coattn': A_coattn, 'path': A_path, 'omic': A_omic}
+
+        return hazards, survs, Y, attention_scores
+
+
+def test_nacagat():
+    print('Testing NarrowGatingContextualAttentionGateTransformer...')
+
+    wsi = torch.randn((3000, 1024))
+    omics = [torch.randn(dim) for dim in [100, 200, 300, 400, 500, 600]]
+    omic_sizes = [omic.size()[0] for omic in omics]
+
+    model_sizes = ['small', 'medium', 'big']
+
+    for model_size in model_sizes:
+        print(f'Size {model_size}')
+        model = NarrowGatingContextualAttentionGateTransformer(omic_sizes=omic_sizes, model_size=model_size)
+        hazards, S, Y_hat, attention_scores = model(wsi, omics)
+        assert hazards.shape[0] == S.shape[0] == Y_hat.shape[0] == 1
+        assert hazards.shape[1] == S.shape[1] == Y_hat.shape[1] == 4
+        assert attention_scores['coattn'].shape[0] == len(omic_sizes)
+        assert attention_scores['coattn'].shape[1] == 3000
+        assert attention_scores['path'].shape[0] == attention_scores['omic'].shape[0] == 1
+        assert attention_scores['path'].shape[1] == attention_scores['omic'].shape[1] == len(omic_sizes)
+        print('Forward successful')

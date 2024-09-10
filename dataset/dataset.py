8adbc9b2cbe491568f32b9363bb833e9412b8c5e
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-09-10 14:29:53 +0300

    Fix cache

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -35,6 +35,10 @@ class MultimodalDatasetV2(Dataset):
         survival_class, _ = pd.qcut(self.data['survival_months'], q=4, retbins=True, labels=False)
         self.data['survival_class'] = survival_class
 
+        self.survival_months = self.data['survival_months'].values
+        self.survival_class = self.data['survival_class'].values
+        self.censorship = self.data['censorship'].values
+
         # RNA
         self.rnaseq = self.data.iloc[:, self.data.columns.str.endswith('_rnaseq')].astype(float)
         if top_rnaseq is not None:
@@ -46,17 +50,17 @@ class MultimodalDatasetV2(Dataset):
         if normalize:
             self.rnaseq = 2 * (self.rnaseq - self.rnaseq.min()) / (self.rnaseq.max() - self.rnaseq.min()) - 1
         print(f'RNA data size: {self.rnaseq_size}')
-        self.rnaseq = torch.tensor(self.rnaseq.values)
+        self.rnaseq = torch.tensor(self.rnaseq.values, dtype=torch.float32)
         # CNV
         self.cnv = self.data.iloc[:, self.data.columns.str.endswith('_cnv')].astype(float)
         self.cnv_size = len(self.cnv.columns)
         print(f'CNV data size: {self.cnv_size}')
-        self.cnv = torch.tensor(self.cnv.values)
+        self.cnv = torch.tensor(self.cnv.values, dtype=torch.float32)
         # MUT
         self.mut = self.data.iloc[:, self.data.columns.str.endswith('_mut')].astype(float)
         self.mut_size = len(self.mut.columns)
         print(f'MUT data size: {self.mut_size}')
-        self.mut = torch.tensor(self.mut.values)
+        self.mut = torch.tensor(self.mut.values, dtype=torch.float32)
 
         # Signatures
         self.use_signatures = use_signatures
@@ -75,38 +79,35 @@ class MultimodalDatasetV2(Dataset):
                     gene += '_rnaseq'
                     if gene in self.data.columns:
                         columns[gene] = self.data[gene]
-                self.signature_data[signature_name] = torch.tensor(pd.DataFrame(columns).values)
+                self.signature_data[signature_name] = torch.tensor(pd.DataFrame(columns).values, dtype=torch.float32)
                 self.signature_sizes.append(self.signature_data[signature_name].shape[1])
             print(f'Signatures size: {self.signature_sizes}')
 
     def __len__(self):
         return len(self.data)
 
-    @lru_cache(maxsize=128)
+    @lru_cache(maxsize=32)
     def load_patch_embedding(self, slide_name):
         return torch.load(os.path.join(self.patches_dir, slide_name))
 
     def __getitem__(self, index):
-        survival_months = self.data['survival_months'][index]
-        survival_class = self.data['survival_class'][index]
-        censorship = self.data['censorship'][index]
+        survival_months = self.survival_months[index]
+        survival_class = self.survival_class[index]
+        censorship = self.censorship[index]
 
         slide_name = self.data['slide_id'][index].replace('.svs', '.pt')
         patches_embeddings = self.load_patch_embedding(slide_name)
 
         if not self.use_signatures:
-            rnaseq = self.rnaseq[index].type(torch.float32)
-            cnv = self.cnv[index].type(torch.float32)
-            mut = self.mut[index].type(torch.float32)
             omics_data = {
-                'rnaseq': rnaseq,
-                'cnv': cnv,
-                'mut': mut
+                'rnaseq': self.rnaseq[index],
+                'cnv': self.cnv[index],
+                'mut': self.mut[index]
             }
         else:
             omics_data = []
             for signature in self.signatures:
-                signature_data = self.signature_data[signature][index].type(torch.float32)
+                signature_data = self.signature_data[signature][index]
                 omics_data.append(signature_data)
 
         return survival_months, survival_class, censorship, omics_data, patches_embeddings

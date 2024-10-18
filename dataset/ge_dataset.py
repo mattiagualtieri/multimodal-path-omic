6c4f3415b6617f4185c376794ca9e098f9b5f14a
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-10-18 13:28:34 +0300

    Added GeneExpr-NaCAGaT

---
+++ dataset/ge_dataset.py
@@ -0,0 +1,238 @@
+import torch
+import os
+import pandas as pd
+import numpy as np
+import h5py
+
+from torch.utils.data import Dataset
+from scipy import stats
+
+
+class MultimodalGeneExprPredDataset(Dataset):
+    def __init__(self, file: str, config, gene: str, use_signatures=False, top_rnaseq=None, remove_incomplete_samples=True, inference=False, standardize=False, normalize=False):
+        self.data = pd.read_csv(file)
+
+        if config['dataset']['decider_only']:
+            print('Using DECIDER data only')
+            self.data = self.data.loc[self.data['is_decider'] == 1.0]
+            self.data.reset_index(drop=True, inplace=True)
+
+        if inference:
+            self.patches_dir = config['inference']['dataset']['patches_dir']
+        else:
+            self.patches_dir = config['dataset']['patches_dir']
+        if self.patches_dir is None:
+            self.patches_dir = ''
+
+        self.use_h5_dataset = False
+        try:
+            self.use_h5_dataset = config['dataset']['h5_dataset'] is not None
+        except KeyError:
+            pass
+
+        if remove_incomplete_samples:
+            slide_index = 0
+            complete_data_only = []
+            if not self.use_h5_dataset:
+                for slide in self.data['slide_id']:
+                    slide_name = slide.replace('.svs', '.pt')
+                    if os.path.exists(os.path.join(self.patches_dir, slide_name)):
+                        complete_data_only.append(self.data.iloc[slide_index])
+                    slide_index += 1
+            else:
+                self.h5_dataset = config['dataset']['h5_dataset']
+                self.h5_file = h5py.File(self.h5_dataset, 'r')
+                for slide in self.data['slide_id']:
+                    slide_name = slide.replace('.svs', '')
+                    if slide_name in self.h5_file:
+                        complete_data_only.append(self.data.iloc[slide_index])
+                    slide_index += 1
+
+            self.data = pd.DataFrame(complete_data_only)
+            self.data.reset_index(drop=True, inplace=True)
+            print(f'Remaining samples after removing incomplete: {len(self.data)}')
+
+        print(f'Testing gene expression: {gene}')
+        self.gene_expr_value = self.data[f'{gene}_rnaseq']
+
+        # Prova con norm tra 0 e 10
+        min_val = self.gene_expr_value.min()
+        max_val = self.gene_expr_value.max()
+        self.gene_expr_value = 10 * (self.gene_expr_value - min_val) / (max_val - min_val)
+
+        self.data = self.data.drop(f'{gene}_rnaseq', axis=1)
+
+        # RNA
+        self.rnaseq = self.data.iloc[:, self.data.columns.str.endswith('_rnaseq')].astype(float)
+        if top_rnaseq is not None:
+            rnaseq = self.data[self.data.columns[self.data.columns.str.contains('_rnaseq')]]
+            mad = stats.median_abs_deviation(rnaseq, axis=0)
+            sort_idx = np.argsort(mad)[-top_rnaseq:]
+            self.rnaseq = rnaseq[rnaseq.columns[sort_idx]]
+        self.rnaseq_size = len(self.rnaseq.columns)
+        if standardize:
+            self.rnaseq = (self.rnaseq - self.rnaseq.mean()) / self.rnaseq.std()
+        if normalize:
+            self.rnaseq = 2 * (self.rnaseq - self.rnaseq.min()) / (self.rnaseq.max() - self.rnaseq.min()) - 1
+        # print(f'RNA data size: {self.rnaseq_size}')
+        self.rnaseq = torch.tensor(self.rnaseq.values, dtype=torch.float32)
+        # CNV
+        self.cnv = self.data.iloc[:, self.data.columns.str.endswith('_cnv')].astype(float)
+        self.cnv_size = len(self.cnv.columns)
+        # print(f'CNV data size: {self.cnv_size}')
+        self.cnv = torch.tensor(self.cnv.values, dtype=torch.float32)
+        # MUT
+        self.mut = self.data.iloc[:, self.data.columns.str.endswith('_mut')].astype(float)
+        self.mut_size = len(self.mut.columns)
+        # print(f'MUT data size: {self.mut_size}')
+        self.mut = torch.tensor(self.mut.values, dtype=torch.float32)
+
+        # Signatures
+        self.use_signatures = use_signatures
+        if self.use_signatures:
+            self.signature_sizes = []
+            self.signature_data = {}
+            if inference:
+                signatures_file = config['inference']['dataset']['signatures']
+            else:
+                signatures_file = config['dataset']['signatures']
+            signatures_df = pd.read_csv(signatures_file)
+            self.signatures = signatures_df.columns
+            for signature_name in self.signatures:
+                columns = {}
+                for gene in signatures_df[signature_name].dropna():
+                    gene += '_rnaseq'
+                    if gene in self.data.columns:
+                        columns[gene] = self.data[gene]
+                self.signature_data[signature_name] = torch.tensor(pd.DataFrame(columns).values, dtype=torch.float32)
+                self.signature_sizes.append(self.signature_data[signature_name].shape[1])
+            print(f'Signatures size: {self.signature_sizes}')
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        gene_expr_value = self.gene_expr_value[index]
+
+        if not self.use_h5_dataset:
+            slide_name = self.data['slide_id'][index].replace('.svs', '.pt')
+            patches_embeddings = torch.load(os.path.join(self.patches_dir, slide_name))
+        else:
+            slide_name = self.data['slide_id'][index].replace('.svs', '')
+            patches_embeddings = torch.tensor(self.h5_file[slide_name])
+
+        if not self.use_signatures:
+            omics_data = {
+                'rnaseq': self.rnaseq[index],
+                'cnv': self.cnv[index],
+                'mut': self.mut[index]
+            }
+        else:
+            omics_data = []
+            for signature in self.signatures:
+                signature_data = self.signature_data[signature][index]
+                omics_data.append(signature_data)
+
+        return gene_expr_value, omics_data, patches_embeddings
+
+    def split(self, train_size, test: bool = False, patient: str = ''):
+        # Ensure train_size is a valid ratio
+        if not 0 < train_size < 1:
+            raise ValueError("train_size should be a float between 0 and 1.")
+
+        # Get unique patients
+        unique_patients = self.data['patient'].unique()
+
+        # Shuffle patients randomly
+        np.random.shuffle(unique_patients)
+
+        # Determine the number of patients in the train split
+        train_patient_count = int(len(unique_patients) * train_size)
+
+        # Split patients into train and test sets
+        train_patients = unique_patients[:train_patient_count]
+        val_patients = unique_patients[train_patient_count:]
+
+        # Filter the data into train and test sets
+        test_dataset = None
+        if test:
+            train_data = self.data[self.data['patient'].isin(train_patients)].copy()
+            train_data = train_data[train_data['patient'] != patient]
+            val_data = self.data[self.data['patient'].isin(val_patients)].copy()
+            val_data = val_data[val_data['patient'] != patient]
+            test_data = self.data[self.data['patient'] == patient].copy()
+            test_data.reset_index(drop=True, inplace=True)
+            test_dataset = MultimodalGeneExprPredDataset.from_dataframe(test_data, self)
+        else:
+            train_data = self.data[self.data['patient'].isin(train_patients)].copy()
+            val_data = self.data[self.data['patient'].isin(val_patients)].copy()
+
+        # Reset indices for train and test datasets
+        train_data.reset_index(drop=True, inplace=True)
+        val_data.reset_index(drop=True, inplace=True)
+
+        # Create new instances of MultimodalDataset with the train and test data
+        train_dataset = MultimodalGeneExprPredDataset.from_dataframe(train_data, self)
+        val_dataset = MultimodalGeneExprPredDataset.from_dataframe(val_data, self)
+
+        return train_dataset, val_dataset, test_dataset
+
+    @classmethod
+    def from_dataframe(cls, df, original_instance):
+        # Create a new MultimodalDataset instance from an existing DataFrame
+        # while preserving the original configuration and parameters
+        instance = cls.__new__(cls)  # Create a new instance without calling __init__
+
+        # Reset indices in the DataFrame
+        df = df.reset_index(drop=True)
+        # Copy attributes from the original instance
+        instance.data = df
+        instance.patches_dir = original_instance.patches_dir
+        instance.use_h5_dataset = original_instance.use_h5_dataset
+        instance.use_signatures = original_instance.use_signatures
+        if original_instance.use_signatures:
+            instance.signatures = original_instance.signatures
+            instance.signature_sizes = original_instance.signature_sizes
+        instance.h5_dataset = original_instance.h5_dataset if original_instance.use_h5_dataset else None
+
+        if original_instance.use_h5_dataset:
+            instance.h5_file = h5py.File(instance.h5_dataset, 'r')
+
+        # Copy RNA, CNV, and MUT data with the new subset of data
+        instance.gene_expr_value = original_instance.gene_expr_value
+
+        # RNA
+        rnaseq = df.iloc[:, df.columns.str.endswith('_rnaseq')].astype(float)
+        if original_instance.rnaseq_size == len(rnaseq.columns):
+            instance.rnaseq = torch.tensor(rnaseq.values, dtype=torch.float32)
+        else:
+            instance.rnaseq = torch.tensor(np.zeros((len(df), original_instance.rnaseq_size)), dtype=torch.float32)
+
+        # CNV
+        cnv = df.iloc[:, df.columns.str.endswith('_cnv')].astype(float)
+        if original_instance.cnv_size == len(cnv.columns):
+            instance.cnv = torch.tensor(cnv.values, dtype=torch.float32)
+        else:
+            instance.cnv = torch.tensor(np.zeros((len(df), original_instance.cnv_size)), dtype=torch.float32)
+
+        # MUT
+        mut = df.iloc[:, df.columns.str.endswith('_mut')].astype(float)
+        if original_instance.mut_size == len(mut.columns):
+            instance.mut = torch.tensor(mut.values, dtype=torch.float32)
+        else:
+            instance.mut = torch.tensor(np.zeros((len(df), original_instance.mut_size)), dtype=torch.float32)
+
+        # Copy signature data if use_signatures is True
+        if original_instance.use_signatures:
+            instance.signature_sizes = original_instance.signature_sizes
+            instance.signature_data = {}
+            for signature_name in original_instance.signatures:
+                signature_data = original_instance.signature_data[signature_name]
+                indices = df.index
+                instance.signature_data[signature_name] = signature_data[indices]
+
+        return instance
+
+    def __del__(self):
+        if self.use_h5_dataset:
+            self.h5_file.close()
---
+++ models/ge_nacagat/main.py
@@ -0,0 +1,307 @@
+import torch.cuda
+import yaml
+import os
+import time
+import datetime
+import wandb
+import torch.nn as nn
+import torch.optim.lr_scheduler as lrs
+
+from torch.utils.data import DataLoader
+from models.utils import l1_reg
+from ge_nacagat import GeneExprNarrowContextualAttentionGateTransformer
+from dataset.ge_dataset import MultimodalGeneExprPredDataset
+
+
+def train(epoch, config, device, train_loader, model, loss_function, optimizer, scheduler, reg_function):
+    model.train()
+    grad_acc_step = config['training']['grad_acc_step']
+    use_scheduler = config['training']['scheduler']
+    lambda_reg = config['training']['lambda']
+    checkpoint_epoch = config['model']['checkpoint_epoch']
+    train_loss = 0.0
+    start_batch_time = time.time()
+    for batch_index, (gene_expr_value, omics_data, patches_embeddings) in enumerate(
+            train_loader):
+
+        gene_expr_value = gene_expr_value.type(torch.FloatTensor).to(device, non_blocking=True)
+        patches_embeddings = patches_embeddings.to(device)
+        omics_data = [omic_data.to(device) for omic_data in omics_data]
+        output, attention_scores = model(wsi=patches_embeddings, omics=omics_data)
+
+        if config['training']['loss'] == 'mse':
+            loss = loss_function(output, gene_expr_value)
+        else:
+            raise RuntimeError(f'Loss "{config["training"]["loss"]}" not implemented')
+        loss_value = loss.item()
+
+        if reg_function is None:
+            loss_reg = 0
+        else:
+            loss_reg = reg_function(model) * lambda_reg
+
+        train_loss += loss_value + loss_reg
+
+        if (batch_index + 1) % 50 == 0:
+            print('\tbatch: {}, loss: {:.4f}, gene_expr_value: {:.4f}, prediction: {:.4f}'.format(
+                batch_index, loss_value + loss_reg, gene_expr_value.item(), output.item()))
+            end_batch_time = time.time()
+            print('\t\taverage speed: {:.2f}s per batch'.format((end_batch_time - start_batch_time) / 32))
+            start_batch_time = time.time()
+        loss = loss / grad_acc_step + loss_reg
+        loss.backward()
+
+        if (batch_index + 1) % grad_acc_step == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+
+    # Calculate loss and error for epoch
+    train_loss /= len(train_loader)
+
+    if use_scheduler:
+        lr = optimizer.param_groups[0]["lr"]
+        scheduler.step()
+        print('Epoch: {}, lr: {:.8f}, train_loss: {:.4f}'.format(epoch + 1, lr, train_loss))
+    else:
+        print('Epoch: {}, train_loss: {:.4f}'.format(epoch + 1, train_loss))
+    if checkpoint_epoch > 0:
+        if (epoch + 1) % checkpoint_epoch == 0 and epoch != 0:
+            now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+            filename = f'{config["model"]["name"]}_{config["dataset"]["name"]}_E{epoch + 1}_{now}.pt'
+            checkpoint_dir = config['model']['checkpoint_dir']
+            checkpoint_path = os.path.join(checkpoint_dir, filename)
+            print(f'Saving model into {checkpoint_path}')
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': train_loss,
+            }, checkpoint_path)
+    wandb_enabled = config['wandb']['enabled']
+    if wandb_enabled:
+        wandb.log({"train_loss": train_loss, "train_mse": train_loss})
+
+
+def validate(epoch, config, device, val_loader, model, loss_function, reg_function):
+    model.eval()
+    val_loss = 0.0
+    lambda_reg = config['training']['lambda']
+    for batch_index, (gene_expr_value, omics_data, patches_embeddings) in enumerate(
+            val_loader):
+        gene_expr_value = gene_expr_value.type(torch.FloatTensor).to(device)
+        patches_embeddings = patches_embeddings.to(device)
+        omics_data = [omic_data.to(device) for omic_data in omics_data]
+        with torch.no_grad():
+            output, attention_scores = model(wsi=patches_embeddings, omics=omics_data)
+
+        if config['training']['loss'] == 'mse':
+            loss = loss_function(output, gene_expr_value)
+        else:
+            raise RuntimeError(f'Loss "{config["training"]["loss"]}" not implemented')
+        loss_value = loss.item()
+
+        if reg_function is None:
+            loss_reg = 0
+        else:
+            loss_reg = reg_function(model) * lambda_reg
+
+        val_loss += loss_value + loss_reg
+
+    # calculate loss and error
+    val_loss /= len(val_loader)
+    if epoch == 'final validation':
+        print('Epoch: {}, val_loss: {:.4f}'.format(epoch, val_loss))
+    else:
+        print('Epoch: {}, val_loss: {:.4f}'.format(epoch + 1, val_loss))
+    wandb_enabled = config['wandb']['enabled']
+    if wandb_enabled:
+        wandb.log({"val_loss": val_loss, "val_mse": val_loss})
+
+
+def test(config, device, epoch, val_loader, model, patient, save=False):
+    model.eval()
+    output_dir = config['training']['test_output_dir']
+    now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    for batch_index, (gene_expr_value, omics_data, patches_embeddings) in enumerate(
+            val_loader):
+
+        gene_expr_value = gene_expr_value.type(torch.FloatTensor).to(device)
+        patches_embeddings = patches_embeddings.to(device)
+        omics_data = [omic_data.to(device) for omic_data in omics_data]
+        print(f'[{batch_index}] Gene Expression Value: {gene_expr_value.item()}')
+        with torch.no_grad():
+            output, attention_scores = model(wsi=patches_embeddings, omics=omics_data)
+            print(f'Prediction: {output.item()}')
+            print(f'Attn min: {attention_scores["coattn"].min()}, Attn max: {attention_scores["coattn"].max()}, Attn '
+                  f'mean: {attention_scores["coattn"].mean()}')
+
+            if save:
+                output_file = os.path.join(output_dir, f'ATTN_{patient}_{now}_E{epoch}_{batch_index}.pt')
+                print(f'Saving attention in {output_file}')
+                torch.save(attention_scores['coattn'], output_file)
+
+
+def wandb_init(config):
+    wandb.init(
+        project=config['wandb']['project'],
+        settings=wandb.Settings(
+            init_timeout=300,
+        ),
+        config={
+            'model': config['model']['name'],
+            'dataset': config['dataset']['name'],
+            'optimizer': config['training']['optimizer'],
+            'learning_rate': config['training']['lr'],
+            'weight_decay': config['training']['weight_decay'],
+            'gradient_acceleration_step': config['training']['grad_acc_step'],
+            'epochs': config['training']['epochs'],
+            'architecture': config['model']['name'],
+            'fusion': config['model']['fusion'],
+            'loss': config['training']['loss'],
+            'scheduler': config['training']['scheduler'],
+            'alpha': config['training']['alpha'],
+            'lambda': config['training']['lambda'],
+            'gamma': config['training']['gamma'],
+            'model_size': config['model']['model_size'],
+            'normalization': config['dataset']['normalize'],
+            'standardization': config['dataset']['standardize'],
+            'leave_one_out': config['training']['leave_one_out']
+        }
+    )
+
+
+def main(config_path: str):
+    with open(config_path) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    wandb_enabled = config['wandb']['enabled']
+    if wandb_enabled:
+        print('Setting up wandb for report')
+        os.environ['WANDB__SERVICE_WAIT'] = '300'
+        wandb_init(config)
+
+    device = config['device']
+    if device == 'cuda' and not torch.cuda.is_available():
+        print('CUDA not available')
+        device = 'cpu'
+    elif device == 'cuda' and torch.cuda.is_available():
+        print('CUDA is available!')
+        print(f'Device count: {torch.cuda.device_count()}')
+        for device_index in range(torch.cuda.device_count()):
+            print(f'Using device: {torch.cuda.get_device_name(device_index)}')
+    print(f'Running on {device.upper()}')
+
+    # Dataset
+    file_csv = config['dataset']['file']
+    normalize = config['dataset']['normalize']
+    standardize = config['dataset']['standardize']
+    print(f'Normalization: {normalize}, Standardization: {standardize}')
+    gene = config['model']['gene']
+    dataset = MultimodalGeneExprPredDataset(file_csv, config, gene=gene, use_signatures=True, normalize=normalize, standardize=standardize)
+    leave_one_out = config['training']['leave_one_out'] is not None
+    train_size = config['training']['train_size']
+    print(f'Using {int(train_size * 100)}% train, {100 - int(train_size * 100)}% validation')
+    test_patient = config['training']['leave_one_out']
+    train_dataset, val_dataset, test_dataset = dataset.split(train_size, test=leave_one_out, patient=test_patient)
+    print(f'Samples in train: {len(train_dataset)}, Samples in validation: {len(val_dataset)}')
+    if test_dataset is not None:
+        print(f'Testing patient {test_patient}')
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
+    output_attn_epoch = config['training']['output_attn_epoch']
+    # Model
+    model_size = config['model']['model_size']
+    omics_sizes = dataset.signature_sizes
+    fusion = config['model']['fusion']
+    model_name = config['model']['name']
+    model = GeneExprNarrowContextualAttentionGateTransformer(model_size=model_size, omic_sizes=omics_sizes, fusion=fusion, device=device)
+    print(f'Trainable parameters of {model_name}: {model.get_trainable_parameters()}')
+    checkpoint_path = config['model']['load_from_checkpoint']
+    checkpoint = None
+    if checkpoint_path is not None:
+        print(f'Loading model checkpoint from {checkpoint_path}')
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    if device == 'cuda' and torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    model.to(device=device)
+    alpha = config['training']['alpha']
+    # Loss function
+    if config['training']['loss'] == 'mse':
+        print('Using MSELoss during training')
+        loss_function = nn.MSELoss()
+    else:
+        raise RuntimeError(f'Loss "{config["training"]["loss"]}" not implemented')
+    # Optimizer
+    lr = config['training']['lr']
+    weight_decay = config['training']['weight_decay']
+    optimizer_name = config['training']['optimizer']
+    if optimizer_name == 'sgd':
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()),
+                                    lr=lr)
+    elif optimizer_name == 'adadelta':
+        optimizer = torch.optim.Adadelta(filter(lambda p: p.requires_grad, model.parameters()),
+                                         lr=lr, weight_decay=weight_decay)
+    elif optimizer_name == 'adamax':
+        optimizer = torch.optim.Adamax(filter(lambda p: p.requires_grad, model.parameters()),
+                                       lr=lr, weight_decay=weight_decay)
+    else:
+        optimizer_name = 'adam'
+        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                                     lr=lr, weight_decay=weight_decay)
+    print(f'Using optimizer: {optimizer_name}')
+
+    scheduler = config['training']['scheduler']
+    if scheduler == 'exp':
+        gamma = config['training']['gamma']
+        scheduler = lrs.ExponentialLR(optimizer, gamma=gamma)
+    else:
+        scheduler = None
+
+    starting_epoch = 0
+    if checkpoint_path is not None:
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        starting_epoch = checkpoint['epoch']
+
+    lambda_param = config['training']['lambda']
+    if lambda_param:
+        reg_function = l1_reg
+    else:
+        reg_function = None
+
+    print('Training started...')
+    model.train()
+    epochs = config['training']['epochs']
+    for epoch in range(starting_epoch, epochs):
+        print(f'Epoch: {epoch + 1}')
+        start_time = time.time()
+        train(epoch, config, device, train_loader, model, loss_function, optimizer, scheduler, reg_function)
+        validate(epoch, config, device, val_loader, model, loss_function, reg_function)
+        if leave_one_out:
+            save = False
+            if (epoch + 1) % output_attn_epoch == 0:
+                save = True
+            test_patient = config['training']['leave_one_out']
+            test(config, device, epoch + 1, test_loader, model, test_patient, save=save)
+        end_time = time.time()
+        print('Time elapsed for epoch {}: {:.0f}s'.format(epoch + 1, end_time - start_time))
+
+    validate('final validation', config, device, val_loader, model, loss_function, reg_function)
+
+    if wandb_enabled:
+        wandb.finish()
+
+
+if __name__ == '__main__':
+    print(f'[{datetime.datetime.now().strftime("%d/%m/%Y - %H:%M")}] GeneExpr-NaCAGAT main started')
+    main('config/config.yaml')
+    print(f'[{datetime.datetime.now().strftime("%d/%m/%Y - %H:%M")}] GeneExpr-NaCAGAT main finished')
+
+
+def test_main():
+    print('Testing GeneExpr-NaCAGAT main...')
+
+    main('config/config_test.yaml')
+
+    print('Test successful')
